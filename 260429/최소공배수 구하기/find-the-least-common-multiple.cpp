ebfa204d8089a9097cb50e3e854b1e8a12acfe36
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-04-29 11:15:28 +0900

    Title: 최소공배수 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void print(int n, int m){
+    int max;
+    for(int i=1; i<=n; i++){
+        if(n % i == 0 && m % i == 0)
+            max = i;
+    }
+    cout << n*m/max;
+}
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if(n > m){
+        int t = m;
+        m = n;
+        n = t;
+    }
+    print(n, m);
+    return 0;
+}
