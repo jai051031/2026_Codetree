13421770e98a6fb17c74e43c5d3d04238a832f57
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-04-30 23:40:57 +0900

    [Passed] 함수를 이용한 온전수 판별 (8ms, 0MB)

---
+++ 260430/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int f(int a, int b){
+    int cnt = 0;
+    for(int i=a; i<=b; i++){
+        if(i % 2 != 0 && i % 10 != 5 && (i % 3 != 0 || i % 9 == 0))
+            cnt++;
+    }
+    return cnt;
+}
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << f(a, b);
+    return 0;
+}
