08b37f40866193cadfebd8902a340b54be69917e
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-05-19 10:59:30 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (8ms, 0MB)

---
+++ 260519/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+void f(int n){
+    if(n < 1)
+        return;
+    for(int i=0; i<n; i++){
+        cout << "* ";
+    }
+    cout << endl;
+    f(n-1);
+    for(int i=0; i<n; i++){
+        cout << "* ";
+    }
+    cout << endl;
+}
+int main() {
+    int n;
+    cin >> n;
+    f(n);
+    return 0;
+}
