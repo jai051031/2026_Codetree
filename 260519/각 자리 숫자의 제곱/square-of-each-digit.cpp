41b9d1b84d2be9e5c98f1b639bf076dc40bce8f4
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-05-19 11:19:30 +0900

    [Passed] 각 자리 숫자의 제곱 (1ms, 0MB)

---
+++ 260519/각 자리 숫자의 제곱/square-of-each-digit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int f(int n){
+    if(n < 10)
+        return n*n;
+    return (n%10)*(n%10)+f(n/10);
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << f(n);
+    return 0;
+}
