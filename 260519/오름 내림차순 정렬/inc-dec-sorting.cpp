4ca2a84315e255c321763d175d1bcc944345805f
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-05-19 14:20:01 +0900

    [Passed] 오름 내림차순 정렬 (2ms, 0MB)

---
+++ 260519/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main() {
+    int n, a[100];
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    sort(a, a+n, greater<int>());
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
