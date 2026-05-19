4c44c5295bf0ca472f0bed2970dfbc1b7ac1297d
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-05-19 13:42:48 +0900

    [Passed] 재귀함수를 이용한 3N + 1 수열 (2ms, 0MB)

---
+++ 260519/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int f(int n){
+    if(n == 1)
+        return 0;
+    if(n % 2 == 0)
+        return f(n/2) + 1;
+    else
+        return f(n*3+1) + 1;
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << f(n);
+    return 0;
+}
