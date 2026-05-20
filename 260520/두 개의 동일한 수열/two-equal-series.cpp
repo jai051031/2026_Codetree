43267da59a0c7dacdd35b42c248c251ae1161dbe
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-05-20 11:53:43 +0900

    [Passed] 두 개의 동일한 수열 (2ms, 0MB)

---
+++ 260520/두 개의 동일한 수열/two-equal-series.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, a[100], b[100], check = 0;
+    cin >> n;
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+    for(int i=0; i<n; i++)
+        cin >> b[i];
+    sort(a, a+n);
+    sort(b, b+n);
+    for(int i=0; i<n; i++){
+        if(a[i] != b[i]){
+            cout << "No";
+            return 0;
+        }
+    }
+    cout << "Yes";
+    return 0;
+}
