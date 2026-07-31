77180fb703294e81b28a66a6b501217e52e3333a
jai051031 <161295087+jai051031@users.noreply.github.com>
2026-07-31 13:29:54 +0900

    [Passed] 일렬로 서있는 소 2 (2ms, 0MB)

---
+++ 260731/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a[100], result = 0;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            if(a[j] >= a[i]){
+                for(int k=j+1; k<n; k++){
+                    if(a[k] >= a[j]){
+                        result++;
+                    }
+                }
+            }
+        }
+    }
+    cout << result;
+    return 0;
+}
